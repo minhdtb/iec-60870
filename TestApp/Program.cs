cbab8b6572fce64881ad88fa1620815520a7d73b
minhdtb83 <minhdtb@atmarkcafe.org>
2015-11-24 13:51:50 +0700

    add test app

---
+++ TestApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using IEC60870.Connection;
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket socket = connect("localhost", 2404);
+            Connection connection = new Connection(socket, new ConnectionSettings());
+        }
+
+        static Socket connect(String host, int port)
+        {
+            IPAddress[] IPs = Dns.GetHostAddresses(host);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(IPs[1], port);
+            return socket;
+        }
+    }
+}

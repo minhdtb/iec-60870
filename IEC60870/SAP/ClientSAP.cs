8b78a3ae0c809febccf6de33fdc7d9dcfe333730
minhdtb <minhdtb.83@gmail.com>
2016-01-17 21:14:26 +0700

    add client SAP

---
+++ IEC60870/SAP/ClientSAP.cs
@@ -0,0 +1,145 @@
+﻿using IEC60870.Connection;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace IEC60870.SAP
+{
+    public class ClientSAP
+    {
+        private ConnectionSettings settings = new ConnectionSettings();
+        private Connection.Connection _connection;
+        private IPAddress _host;
+        private int _port;
+
+        public ConnectionEventListener.NewASdu NewASdu { get; set; }
+        public ConnectionEventListener.ConnectionClosed ConnectionClosed { get; set; }
+
+        public ClientSAP(IPAddress host)
+        {
+            _host = host;
+            _port = 2404;
+        }
+
+        public ClientSAP(IPAddress host, int port) : this(host)
+        {
+            _port = port;
+        }
+
+        public ClientSAP(string host, int port)
+        {
+            try
+            {
+                _host = IPAddress.Parse(host);
+                _port = port;
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message);
+            }            
+        }
+
+        public void Connect()
+        {            
+            try
+            {
+                var remoteEp = new IPEndPoint(_host, _port);
+                var socket = new Socket(_host.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(remoteEp);
+
+                _connection = new Connection.Connection(socket, settings);
+                _connection.NewASdu = NewASdu ?? null;
+                _connection.ConnectionClosed = ConnectionClosed ?? null;
+                _connection.StartDataTransfer();
+            }
+            catch (Exception e)
+            {
+                throw new IOException(e.Message);
+            }            
+        }      
+
+        public void SetMessageFragmentTimeout(int timeout)
+        {
+            if (timeout < 0)
+            {
+                throw new ArgumentException("Invalid message fragment timeout: " + timeout);
+            }
+
+            settings.MessageFragmentTimeout = timeout;
+        }
+
+        public void SetCotFieldLength(int length)
+        {
+            if (length != 1 && length != 2)
+            {
+                throw new ArgumentException("Invalid COT length: " + length);
+            }
+
+            settings.CotFieldLength = length;
+        }
+
+        public void SetCommonAddressFieldLength(int length)
+        {
+            if (length != 1 && length != 2)
+            {
+                throw new ArgumentException("Invalid CA length: " + length);
+            }
+
+            settings.CommonAddressFieldLength = length;
+        }
+
+        public void SetIoaFieldLength(int length)
+        {
+            if (length < 1 || length > 3)
+            {
+                throw new ArgumentException("Invalid IOA length: " + length);
+            }
+
+            settings.IoaFieldLength = length;
+        }
+
+        public void SetMaxTimeNoAckReceived(int time)
+        {
+            if (time < 1000 || time > 255000)
+            {
+                throw new ArgumentException("Invalid NoACK received timeout: " + time
+                        + ", time must be between 1000ms and 255000ms");
+            }
+
+            settings.MaxTimeNoAckReceived = time;
+        }
+
+        public void SetMaxTimeNoAckSent(int time)
+        {
+            if (time < 1000 || time > 255000)
+            {
+                throw new ArgumentException("Invalid NoACK sent timeout: " + time
+                        + ", time must be between 1000ms and 255000ms");
+            }
+
+            settings.MaxTimeNoAckSent = time;
+        }
+
+        public void SetMaxIdleTime(int time)
+        {
+            if (time < 1000 || time > 172800000)
+            {
+                throw new ArgumentException("Invalid idle timeout: " + time
+                        + ", time must be between 1000ms and 172800000ms");
+            }
+
+            settings.MaxIdleTime = time;
+        }
+
+        public void SetMaxUnconfirmedIPdusReceived(int maxNum)
+        {
+            if (maxNum < 1 || maxNum > 32767)
+            {
+                throw new ArgumentException("invalid maxNum: " + maxNum + ", must be a value between 1 and 32767");
+            }
+
+            settings.MaxUnconfirmedIPdusReceived = maxNum;
+        }
+    }
+}
--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Net;
-using System.Net.Sockets;
-using IEC60870.Connection;
+using IEC60870.SAP;
 
 namespace TestApp
 {
@@ -11,18 +9,14 @@ namespace TestApp
         {
             try
             {
-                var socket = Connect("127.0.0.1", 2404);
-                var settings = new ConnectionSettings();
+                var client = new ClientSAP("127.0.0.1", 2404);
+                client.NewASdu += asdu => {
+                    Console.WriteLine(asdu);
+                };
 
-                var connection = new Connection(socket, settings);
-
-                connection.NewASdu += asdu => { Console.WriteLine("\nReceived ASDU:\n" + asdu.ToString()); };
-
-                connection.ConnectionClosed += Console.WriteLine;
-
-                connection.StartDataTransfer();
+                client.Connect();                
             }
-            catch (Exception e)
+            catch(Exception e)
             {
                 Console.WriteLine(e);
             }
@@ -30,15 +24,5 @@ namespace TestApp
             Console.ReadLine();
         }
 
-        private static Socket Connect(string host, int port)
-        {
-            var ipAddress = IPAddress.Parse(host);
-            var remoteEp = new IPEndPoint(ipAddress, port);
-
-            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(remoteEp);
-
-            return socket;
-        }
     }
 }

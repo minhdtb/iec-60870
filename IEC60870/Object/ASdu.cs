3b0e365aca9f3253926a0370330a7ce154f3d222
minhdtb <minhdtb.83@gmail.com>
2017-05-28 18:48:35 +0700

    revert commit da48675eebc81b047e8a0c75e220b4fe7dc4d966

--- IEC60870/Object/ASdu.cs
+++ IEC60870/Object/ASdu.cs
@@ -230,7 +230,7 @@ namespace IEC60870.Object
                 i += privateInformation.Length;
             }
 
-            return i - origi + 1;
+            return i - origi;
         }
 
         public override string ToString()
